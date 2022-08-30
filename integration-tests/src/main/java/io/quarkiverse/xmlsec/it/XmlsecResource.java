7efc8dbad942883cec67ec6f852423eacdbfbe33
Peter Palaga <ppalaga@redhat.com>
2022-08-30 14:55:32 +0200

    Add a DOM encryption integration test #45

--- integration-tests/src/main/java/io/quarkiverse/xmlsec/it/XmlsecResource.java
+++ integration-tests/src/main/java/io/quarkiverse/xmlsec/it/XmlsecResource.java
@@ -16,17 +16,120 @@
 */
 package io.quarkiverse.xmlsec.it;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.security.Key;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
 import javax.enterprise.context.ApplicationScoped;
-import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.xml.namespace.QName;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.apache.xml.security.utils.XMLUtils;
+import org.w3c.dom.Document;
 
 @Path("/xmlsec")
 @ApplicationScoped
 public class XmlsecResource {
-    // add some rest methods here
 
-    @GET
-    public String hello() {
-        return "Hello xmlsec";
+    private final KeyStore keyStore;
+
+    public XmlsecResource() throws KeyStoreException, NoSuchAlgorithmException, CertificateException, IOException {
+        // Set up the Key
+        keyStore = KeyStore.getInstance("jks");
+        keyStore.load(
+                this.getClass().getClassLoader().getResource("servicestore.jks").openStream(),
+                "sspass".toCharArray());
     }
+
+    /**
+     * Adapted form <a href=
+     * "https://github.com/coheigea/testcases/blob/master/apache/santuario/santuario-xml-encryption/src/test/java/org/apache/coheigea/santuario/xmlencryption/EncryptionDOMTest.java">https://github.com/coheigea/testcases/blob/master/apache/santuario/santuario-xml-encryption/src/test/java/org/apache/coheigea/santuario/xmlencryption/EncryptionDOMTest.java</a>
+     * by <a href="https://github.com/coheigea">Colm O hEigeartaigh</a>
+     *
+     * @param plaintext
+     * @return
+     * @throws Exception
+     */
+    @POST
+    @Path("/dom/encrypt")
+    public byte[] encryptDom(byte[] plaintext) throws Exception {
+        try (ByteArrayInputStream in = new ByteArrayInputStream(plaintext)) {
+            DocumentBuilder builder = createDocumentBuilder(false, false);
+            Document document = builder.parse(in);
+
+            X509Certificate cert = (X509Certificate) keyStore.getCertificate("myservicekey");
+
+            // Set up the secret Key
+            KeyGenerator keygen = KeyGenerator.getInstance("AES");
+            keygen.init(128);
+            SecretKey secretKey = keygen.generateKey();
+
+            // Encrypt using DOM
+            List<QName> namesToEncrypt = new ArrayList<QName>();
+            namesToEncrypt.add(new QName("urn:example:po", "PaymentInfo"));
+            EncryptionUtils.encryptUsingDOM(
+                    document, namesToEncrypt, "http://www.w3.org/2001/04/xmlenc#aes128-cbc", secretKey,
+                    "http://www.w3.org/2001/04/xmlenc#rsa-oaep-mgf1p", cert.getPublicKey(), false);
+
+            try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
+                XMLUtils.outputDOM(document, baos);
+                return baos.toByteArray();
+            }
+        }
+    }
+
+    /**
+     * Adapted form <a href=
+     * "https://github.com/coheigea/testcases/blob/master/apache/santuario/santuario-xml-encryption/src/test/java/org/apache/coheigea/santuario/xmlencryption/EncryptionDOMTest.java">https://github.com/coheigea/testcases/blob/master/apache/santuario/santuario-xml-encryption/src/test/java/org/apache/coheigea/santuario/xmlencryption/EncryptionDOMTest.java</a>
+     * by <a href="https://github.com/coheigea">Colm O hEigeartaigh</a>
+     *
+     * @param encrypted
+     * @return
+     * @throws Exception
+     */
+    @POST
+    @Path("/dom/decrypt")
+    public byte[] decryptDom(byte[] encrypted) throws Exception {
+        try (ByteArrayInputStream in = new ByteArrayInputStream(encrypted)) {
+            DocumentBuilder builder = createDocumentBuilder(false, false);
+            Document document = builder.parse(in);
+
+            // Decrypt using DOM
+            Key privateKey = keyStore.getKey("myservicekey", "skpass".toCharArray());
+            EncryptionUtils.decryptUsingDOM(document,
+                    "http://www.w3.org/2001/04/xmlenc#aes128-cbc", privateKey);
+
+            try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
+                XMLUtils.outputDOM(document, baos);
+                return baos.toByteArray();
+            }
+        }
+    }
+
+    public static DocumentBuilder createDocumentBuilder(
+            boolean validating, boolean disAllowDocTypeDeclarations) throws ParserConfigurationException {
+        DocumentBuilderFactory dfactory = DocumentBuilderFactory.newInstance();
+        dfactory.setFeature(javax.xml.XMLConstants.FEATURE_SECURE_PROCESSING, Boolean.TRUE);
+        if (disAllowDocTypeDeclarations) {
+            dfactory.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
+        }
+        dfactory.setValidating(validating);
+        dfactory.setNamespaceAware(true);
+        return dfactory.newDocumentBuilder();
+    }
+
 }
--- integration-tests/src/test/java/io/quarkiverse/xmlsec/it/XmlsecResourceTest.java
+++ integration-tests/src/test/java/io/quarkiverse/xmlsec/it/XmlsecResourceTest.java
@@ -1,9 +1,26 @@
 package io.quarkiverse.xmlsec.it;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.is;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 import io.quarkus.test.junit.QuarkusTest;
 
@@ -11,11 +28,54 @@ import io.quarkus.test.junit.QuarkusTest;
 public class XmlsecResourceTest {
 
     @Test
-    public void testHelloEndpoint() {
-        given()
-                .when().get("/xmlsec")
-                .then()
-                .statusCode(200)
-                .body(is("Hello xmlsec"));
+    public void encryptDecryptDom() throws IOException, ParserConfigurationException, SAXException, XPathExpressionException {
+        try (InputStream plaintext = getClass().getClassLoader().getResourceAsStream("plaintext.xml");
+                ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
+            IOUtils.copy(plaintext, baos);
+            byte[] plainBytes = baos.toByteArray();
+            byte[] encrypted = given()
+                    .body(plainBytes)
+                    .when()
+                    .post("/xmlsec/dom/encrypt")
+                    .then()
+                    .statusCode(200)
+                    .extract().body().asByteArray();
+            try (ByteArrayInputStream in = new ByteArrayInputStream(encrypted)) {
+
+                DocumentBuilder builder = XmlsecResource.createDocumentBuilder(false, true);
+                Document encryptedDoc = builder.parse(in);
+
+                XPathFactory xpf = XPathFactory.newInstance();
+                XPath xpath = xpf.newXPath();
+                xpath.setNamespaceContext(new DSNamespaceContext());
+                String expression = "//xenc:EncryptedData[1]";
+                Element encElement = (Element) xpath.evaluate(expression, encryptedDoc, XPathConstants.NODE);
+                Assertions.assertNotNull(encElement);
+
+                // Check the CreditCard encrypted ok
+                NodeList nodeList = encryptedDoc.getElementsByTagNameNS("urn:example:po", "CreditCard");
+                Assertions.assertEquals(nodeList.getLength(), 0);
+
+            }
+
+            /* Decrypt */
+            byte[] decrypted = given()
+                    .body(encrypted)
+                    .when()
+                    .post("/xmlsec/dom/decrypt")
+                    .then()
+                    .statusCode(200)
+                    .extract().body().asByteArray();
+            try (ByteArrayInputStream in = new ByteArrayInputStream(decrypted)) {
+
+                DocumentBuilder builder = XmlsecResource.createDocumentBuilder(false, true);
+                Document decryptedDoc = builder.parse(in);
+                // Check the CreditCard decrypted ok
+                NodeList nodeList = decryptedDoc.getElementsByTagNameNS("urn:example:po", "CreditCard");
+                Assertions.assertEquals(nodeList.getLength(), 1);
+            }
+
+        }
+
     }
 }
